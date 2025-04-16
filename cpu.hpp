838643f32c1a7d7c9137d5951f08ccdb5f7d8466
Rishi Gandikota <rgandik@cs.utexas.edu>
2025-04-16 17:28:54 -0500

    Instruction ENUM

--- cpu.hpp
+++ cpu.hpp
@@ -7,6 +7,10 @@
 #include <stdint.h>
 const int FLAGS_REGISTER = 5;
 
+enum INSTRUCTION {
+
+};
+
 class CPU {
     uint8_t regs[8]; // 0: A, 1: B, 2: C, 3: D, 4: E, 5: F, 6: H, 7: L
     uint8_t sp; // Stack Pointer
