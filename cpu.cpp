b20c6a8cb878d5278fbef2125637eef4c01a837b
Rishi Gandikota <rgandik@cs.utexas.edu>
2025-04-17 21:42:35 -0500

    Instance of CPU, decode infrastructure

--- cpu.cpp
+++ cpu.cpp
@@ -3,6 +3,18 @@
  * All operation emulation functions should be defined here.
  */
 
+#include "cpu.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+
+CPU::CPU() {
+    // Register initialization
+    for (size_t i = 0; i < 8; i++)
+        regs[i] = 0;
+    sp = 0;
+    pc = 0;
+}
+
  // DECODE
  // Jai
 
@@ -11,6 +23,41 @@
  // Ella
 
  // Rishi
+ bool CPU::decode_SWAP_98(uint8_t opcode) {
+    bool outcome = (opcode && 0b11111000) == 0b00110000;
+
+    return outcome;
+}
+
+bool CPU::decode_SWAP_99(uint8_t opcode) {
+    bool outcome = opcode == 0x36;
+
+    return outcome;
+}
+
+bool CPU::decode_SRL_100(uint8_t opcode) {
+    bool outcome = (opcode && 0b11111000) == 0b00111000;
+
+    return outcome;
+}
+
+bool CPU::decode_SRL_101(uint8_t opcode) {
+    bool outcome = opcode == 0x3E;
+
+    return outcome;
+}
+
+bool CPU::decode_BIT_102(uint8_t opcode) {
+    bool outcome = (opcode && 0b11000000) == 0b01000000;
+
+    return outcome;
+}
+
+bool CPU::decode_BIT_103(uint8_t opcode) {
+    bool outcome = (opcode && 0b11000111) == 0b01000110;
+
+    return outcome;
+}
 
  // EXECUTE
  // Jai
