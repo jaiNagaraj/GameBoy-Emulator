1fac94f75f8dbbb9353144b49332fcd95db36c34
Jai Nagaraj <jai.v.nagaraj@gmail.com>
2025-04-21 23:15:22 -0500

    Made framework for main files/loop and ppu

--- gb.cpp
+++ gb.cpp
@@ -1,1 +1,10 @@
-;
+#include "gb.hpp"
+
+void GheithBoy::run_gb() {
+    cpu = new CPU();
+    // Use this space to load the ROM
+
+
+    // Use this space to run graphics (will include the main loop)
+    ;
+}
