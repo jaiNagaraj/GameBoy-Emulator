aa9d3639eff9be37ae99090e2d7b76f526783287
Rishi Gandikota <rgandik@cs.utexas.edu>
2025-04-22 23:46:05 -0500

    MMU implementation of memory

--- mmu.hpp
+++ mmu.hpp
@@ -1,30 +1,32 @@
-/**
- * Header file for RAM.hpp - declare all memory operations in the RAM class.
- * It also contains the structures within the RAM (ex. memory, etc)
- */
-
 #pragma once
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "cpu.hpp"
+#include "ppu.hpp"
+#include "RAM.hpp"
 #include "mmap.hpp"
 
-class RAM {
-private:
+class MMU {
+private: 
+    CPU *cpu;
+    PPU *ppu;
+    RAM *ram;
     MMAP *mmap;
 
 public:
-    // Constructor
-    RAM();
+    MMU();
 
+    void connect_cpu(CPU *cpu);
+    void connect_ppu(PPU *ppu);
+    void connect_ram(RAM *ram);
     void connect_mmap(MMAP *mmap);
 
-    // Memory operation
     uint8_t read_mem(uint16_t addr);
     void write_mem(uint16_t addr, uint8_t data);
 
-    // Stack operations
     uint16_t push_stack(uint16_t sp, uint16_t data);
     uint16_t pop_stack(uint16_t sp);
+
 };
