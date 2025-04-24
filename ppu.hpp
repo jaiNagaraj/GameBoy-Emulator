8a7a77f4be1e45fccee0db8895d8cce1ca9377ea
Ella Whitney <ellawhitneyr@gmail.com>
2025-04-24 10:01:06 -0500

    window and sprites

--- ppu.hpp
+++ ppu.hpp
@@ -5,12 +5,6 @@ class PPU {
     const static int SCREEN_WIDTH = 160;
     const static int SCREEN_HEIGHT = 144;
 
-    class Tile {
-        // This is an 8x8 pixel grid with four colors.
-		// The colors are represented as 2 bits per pixel.
-        ;
-    };
-
     enum COLOR {
         TRANSPARENT = 0,
         WHITE = 1,
@@ -24,6 +18,8 @@ class PPU {
     uint8_t SCY_reg;
     uint8_t WX_reg;
     uint8_t WY_reg;
+    uint8_t OBP0_reg;
+    uint8_t OBP1_reg;
 
     COLOR pixelData[SCREEN_HEIGHT][SCREEN_WIDTH];
 	COLOR backgroundData[SCREEN_HEIGHT][SCREEN_WIDTH];
