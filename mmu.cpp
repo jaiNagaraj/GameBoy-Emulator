f25a7d9d146c1af81d285eae09d26d2fcd6e3cd6
Rishi Gandikota <rgandik@cs.utexas.edu>
2025-04-27 14:19:25 -0500

    Added scoping to switch statements

--- mmu.cpp
+++ mmu.cpp
@@ -72,63 +72,81 @@ uint8_t MMU::read_mem(uint16_t addr) {
     // I/O Registers : 0xFF00 - 0xFF7F
     if (addr >= 0xFF00 && addr <= 0xFF7F) {
         switch (addr) {
-            case 0xFF00: // JOYP - Joypad Input Register
-                // TODO: Interrupt handling?
+            case 0xFF00: { // JOYP - Joypad Input Register
                 return input->get_joyp_state(mmap->read_mem(addr));
+            }
 
-            case 0xFF04: // DIV - Divider Register (Timer)
+            case 0xFF04: { // DIV - Divider Register (Timer)
                 return ram->read_mem(addr);
+            }
 
-            case 0xFF05: // TIMA - Timer Counter
+            case 0xFF05: { // TIMA - Timer Counter
                 return ram->read_mem(addr);
+            }
 
-            case 0xFF06: // TMA - Timer Modulo
+            case 0xFF06: { // TMA - Timer Modulo
                 return ram->read_mem(addr);
+            }
 
-            case 0xFF07: // TAC - Timer Control
+            case 0xFF07: { // TAC - Timer Control
                 return ram->read_mem(addr);
+            }
 
-            case 0xFF0F: // IF - Interrupt Flag
+            case 0xFF0F: { // IF - Interrupt Flag
                 return ram->read_mem(addr) | 0xE0; // Top 3 bits always read as 1
+            }
 
-            case 0xFF40: // LCDC - LCD Control
+            case 0xFF40: { // LCDC - LCD Control
                 return ram->read_mem(addr);
+            }
             
-            case 0xFF41: // STAT - LCD Status
+            case 0xFF41: { // STAT - LCD Status
                 return ram->read_mem(addr);
+            }
 
-            case 0xFF42: // SCY - Scroll Y
+            case 0xFF42: { // SCY - Scroll Y
                 return ram->read_mem(addr);
+            }
 
-            case 0xFF43: // SCX - Scroll X
+            case 0xFF43: { // SCX - Scroll X
                 return ram->read_mem(addr);
+            }
 
-            case 0xFF44: // LY - LCD Y-Coordinate (Read-Only, updated by PPU)
+            case 0xFF44: { // LY - LCD Y-Coordinate (Read-Only, updated by PPU)
                 return ram->read_mem(addr);
+            }
 
-            case 0xFF45: // LYC - LY Compare
+            case 0xFF45: { // LYC - LY Compare
                 return ram->read_mem(addr);
+            }
 
-            case 0xFF46: // DMA - DMA Transfer Start Address
+            case 0xFF46: { // DMA - DMA Transfer Start Address
                 return ram->read_mem(addr);
+            }
 
-            case 0xFF47: // BGP - Background Palette Data
+            case 0xFF47: { // BGP - Background Palette Data
                 return ram->read_mem(addr);
+            }
 
-            case 0xFF48: // OBP0 - Object Palette 0 Data
+            case 0xFF48: { // OBP0 - Object Palette 0 Data
                 return ram->read_mem(addr);
+            }
 
-            case 0xFF49: // OBP1 - Object Palette 1 Data
+            case 0xFF49: { // OBP1 - Object Palette 1 Data
                 return ram->read_mem(addr);
+            }
 
-            case 0xFF4A: // WY - Window Y Position
+            case 0xFF4A: { // WY - Window Y Position
                 return ram->read_mem(addr);
+            }
             
-            case 0xFF4B: // WX - Window X Position minus 7
+            case 0xFF4B: { // WX - Window X Position minus 7
                 return ram->read_mem(addr);
+            }
 
-            default:
+            default: {
                 return ram->read_mem(addr);
+            }
         }
     }
 
@@ -207,72 +225,87 @@ void MMU::write_mem(uint16_t addr, uint8_t data) {
     // I/O Registers : 0xFF00 - 0xFF7F
     if (addr >= 0xFF00 && addr <= 0xFF7F) {
         switch (addr) {
-            case 0xFF00: // JOYP - Joypad Input Register
-                // TODO: Interrupt handling?
+            case 0xFF00: { // JOYP - Joypad Input Register
                 ram->write_mem(addr, (mmap->read_mem(addr) & 0xCF) | (data & 0x30));
                 return;
+            }
 
-            case 0xFF04: // DIV - Divider Register (Timer)
+            case 0xFF04: { // DIV - Divider Register (Timer)
                 ram->write_mem(addr, 0); // Any write resets DIV to 0
                 return;
+            }
 
-            case 0xFF05: // TIMA - Timer Counter
+            case 0xFF05: { // TIMA - Timer Counter
                 ram->write_mem(addr, data);
                 return;
+            }
 
-            case 0xFF06: // TMA - Timer Modulo
+            case 0xFF06: { // TMA - Timer Modulo
                 ram->write_mem(addr, data);
                 return;
+            }
 
-            case 0xFF07: // TAC - Timer Control
+            case 0xFF07: { // TAC - Timer Control
                 ram->write_mem(addr, data);
                 return;
+            }
 
-            case 0xFF0F: // IF - Interrupt Flag
+            case 0xFF0F: { // IF - Interrupt Flag
                 ram->write_mem(addr, data); // Restriction on top 3 bits read-enforced
                 return;
+            }
 
-            case 0xFF40: // LCDC - LCD Control
+            case 0xFF40: { // LCDC - LCD Control
                 ram->write_mem(addr, data);
                 return;
+            }
             
-            case 0xFF41: // STAT - LCD Status
+            case 0xFF41: { // STAT - LCD Status
                 ram->write_mem(addr, (ram->read_mem(addr) & 0x87) | (data & 0x78)); // Combine R/O and W bits
                 return;
+            }
 
-            case 0xFF42: // SCY - Scroll Y
+            case 0xFF42: { // SCY - Scroll Y
                 ram->write_mem(addr, data);
                 return;
+            }
 
-            case 0xFF43: // SCX - Scroll X
+            case 0xFF43: { // SCX - Scroll X
                 ram->write_mem(addr, data);
                 return;
+            }
 
-            case 0xFF44: // LY - LCD Y-Coordinate (Read-Only, updated by PPU)
+            case 0xFF44: { // LY - LCD Y-Coordinate (Read-Only, updated by PPU)
                 return;
+            }
 
-            case 0xFF45: // LYC - LY Compare
+            case 0xFF45: { // LYC - LY Compare
                 ram->write_mem(addr, data);
                 return;
+            }
 
-            case 0xFF46: // DMA - DMA Transfer Start Address
+            case 0xFF46: { // DMA - DMA Transfer Start Address
                 // TODO: set DMA transfer flag
                 ram->write_mem(addr, data);
                 return;
+            }
 
-            case 0xFF47: // BGP - Background Palette Data
+            case 0xFF47: { // BGP - Background Palette Data
                 ram->write_mem(addr, data);    
                 return;
+            }
 
-            case 0xFF48: // OBP0 - Object Palette 0 Data
+            case 0xFF48: { // OBP0 - Object Palette 0 Data
                 ram->write_mem(addr, data);    
                 return;
+            }
 
-            case 0xFF49: // OBP1 - Object Palette 1 Data
+            case 0xFF49: { // OBP1 - Object Palette 1 Data
                 ram->write_mem(addr, data);
                 return;
+            }
 
-            case 0xFF4A: // WY - Window Y Position
+            case 0xFF4A: { // WY - Window Y Position
                 uint8_t stat = ram->read_mem(0xFF41);
 
                 if ((stat & 0b00000011) == 0b00000011) { // Mode 3
@@ -281,8 +314,9 @@ void MMU::write_mem(uint16_t addr, uint8_t data) {
                     ram->write_mem(addr, data);
                     return;
                 }
+            }
             
-            case 0xFF4B: // WX - Window X Position minus 7
+            case 0xFF4B: { // WX - Window X Position minus 7
                 uint8_t stat = ram->read_mem(0xFF41);
 
                 if ((stat & 0b00000011) == 0b00000011) { // Mode 3
@@ -291,10 +325,12 @@ void MMU::write_mem(uint16_t addr, uint8_t data) {
                     ram->write_mem(addr, data);
                     return;
                 }
+            }
 
-            default:
+            default: {
                 ram->write_mem(addr, data);
                 return;
+            }
         }
     }
 
